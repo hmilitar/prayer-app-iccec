9c3fd601c176b4b076b72db250ab14c7202ab224
Heinz Militar <heinzmilitar@gmail.com>
2025-09-15 23:17:19 +0800

    fix: daily readings, expo 54, and ui/ux updates

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ActivityIndicator, useColorScheme } from 'react-native';
